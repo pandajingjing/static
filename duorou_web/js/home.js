02e1001d3767bba1e87ebfbb1f0268266af9ebe7
xujing <xujing@jxulife.com>
2017-11-28 11:01:32 +0800

    调整结束

--- duorou_web/js/home.js
+++ duorou_web/js/home.js
@@ -1,19 +1,3 @@
-//$('.grid').masonry({
-//	// options
-//	itemSelector : '.grid-item',
-//	columnWidth : 246,
-//	fitWidth : true,
-//	gutter : 20
-//});
-//
-//$('.grid').infiniteScroll({
-//	// options
-//	path : 'http://duorou.jxu.dev.jxulife.com/home/images/{{#}}',
-//	append : '.grid-item',
-//	history : false,
-//// prefill : true
-//});
-
 var $grid = $('.grid').masonry({
 	// Masonry options...
 	itemSelector : '.grid-item',
